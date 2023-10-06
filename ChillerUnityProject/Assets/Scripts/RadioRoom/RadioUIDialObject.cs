f022278ca06ada3b754d1045d0d831b2b4b20351
gareth-kmet <gareth.kmet@gmail.com>
2023-10-06 14:11:00 -0600

    Made dials work for the radio

---
+++ ChillerUnityProject/Assets/Scripts/RadioRoom/RadioUIDialObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioUIDialObject : DialUIObject
+{
+    private float currentValue = 0;
+
+    protected override float CalcDialOutputValue(float _rot)
+    {
+        currentValue = base.CalcDialOutputValue(_rot);
+        return currentValue;
+    }
+}
