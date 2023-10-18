4ceaa1ebe4e704ac1ea14f4b76fff41e34e0509f
theOldSheep <sunnymengyangliu@gmail.com>
2023-10-18 13:32:55 -0600

    Squashed commit of the following:
    commit 9ed024943ac10e47d701af548344d9e7fa742b45
    Author: theOldSheep <sunnymengyangliu@gmail.com>
    Date:   Wed Oct 18 13:31:48 2023 -0600
    
        new player movement system and removed pipe debug message

--- ChillerUnityProject/Assets/Scripts/Entities/Player.cs
+++ ChillerUnityProject/Assets/Scripts/Entities/Player.cs
@@ -20,7 +20,14 @@ public class Player : Entity {
         }
     }
     // this is the acceleration per second. It may need additional tweaks to yield a satisfying result.
+    // DEPRECATED: currently a new movement system is used
     public float MOVE_ACCELERATION_PER_SECOND = 2.5f;
+    // the movement is continuous: a movement direction is specified, then the player moves
+    // for MOVE_DURATION_SEC amont of time, move according to the moving direction with speed MOVE_SPEED.
+    // this system is mainly implemented to prevent funny sprite twitch when the player quickly alternates between A and D 
+    public float MOVE_DURATION_SEC = 0.1f, MOVE_SPEED = 1;
+    public float movement_progress = 0f;
+    public Vector3 moveDir = Vector3.zero;
 
     // animation sprite lists
     public Sprite[] idleAnim;
@@ -43,29 +50,59 @@ public class Player : Entity {
     }
     
     protected override void AI() {
-        // positive: right & up
-        float horMoveDir = 0, verMoveDir = 0;
+        // update velocity (otherwise, the player seems to slide after movement ends)
+        this.velocity = moveDir;
+        // update movement direction if idle
+        if (movement_progress == 0f) {
+            // positive: right & up
+            float horMoveDir = 0, verMoveDir = 0;
 
-        // sprite animation changes depending on which key is pressed
-        if (Settings.Controls.MoveUp.GetKey()) {
-            spriteAnimations[0].ChangeAnimation(walkNorthAnim, false);
-            verMoveDir ++;
-        } else if (Settings.Controls.MoveDown.GetKey()) {
-            spriteAnimations[0].ChangeAnimation(walkSouthAnim, false);
-            verMoveDir --;
-        } else if (Settings.Controls.MoveLeft.GetKey()) {
-            spriteAnimations[0].ChangeAnimation(walkWestAnim, false);
-            horMoveDir --;
-        } else if (Settings.Controls.MoveRight.GetKey()) {
-            spriteAnimations[0].ChangeAnimation(walkEastAnim, false);
-            horMoveDir ++;
-        } else {
-            // idle animation
-            spriteAnimations[0].ChangeAnimation(idleAnim);
+            // sprite animation changes depending on which key is pressed
+            if (Settings.Controls.MoveUp.GetKey()) {
+                verMoveDir ++;
+            }
+            if (Settings.Controls.MoveDown.GetKey()) {
+                verMoveDir --;
+            }
+            if (Settings.Controls.MoveLeft.GetKey()) {
+                horMoveDir --;
+            }
+            if (Settings.Controls.MoveRight.GetKey()) {
+                horMoveDir ++;
+            }
+            
+            moveDir = new Vector3(horMoveDir, verMoveDir, 0);
+            moveDir *= MOVE_SPEED;
+            // start movement if velocity is non-zero
+            if (moveDir.sqrMagnitude > 1e-5)
+                movement_progress = 1e-9f;
         }
-        // accelerate
-        Vector3 accel = new Vector3(horMoveDir * MOVE_ACCELERATION_PER_SECOND, verMoveDir * MOVE_ACCELERATION_PER_SECOND, 0);
-        accel *= Time.deltaTime;
-        accelerate(accel, -1);
+        // update sprite animation
+        {
+            // north
+            if (moveDir.y > 1e-5)
+                spriteAnimations[0].ChangeAnimation(walkNorthAnim, false);
+            // south
+            else if (moveDir.y < -1e-5)
+                spriteAnimations[0].ChangeAnimation(walkSouthAnim, false);
+            // east
+            else if (moveDir.x > 1e-5)
+                spriteAnimations[0].ChangeAnimation(walkEastAnim, false);
+            // west 
+            else if (moveDir.x < -1e-5)
+                spriteAnimations[0].ChangeAnimation(walkWestAnim, false);
+            // idle
+            else
+                spriteAnimations[0].ChangeAnimation(idleAnim);
+        }
+        // move until timeout, then wait for next action
+        if (movement_progress > 0f) {
+            movement_progress += Time.deltaTime;
+            // timeout
+            if (movement_progress >= MOVE_DURATION_SEC) {
+                movement_progress = 0f;
+                moveDir = Vector3.zero;
+            }
+        }
     }
 }
