e186b00b2d66ec3f8774b16290f61ef09855e9b1
theOldSheep <sunnymengyangliu@gmail.com>
2023-10-28 00:00:09 -0600

    UI ghost

---
+++ ChillerUnityProject/Assets/Scripts/UISpecificObjects/visions/GhostVisionUIObjectAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * the animator for ghost vision UI obj
+ * REMEMBER TO INSERT ONE MORE ITEM INTO EACH SPRITE ARRAY
+ */
+public class GhostVisionUIObjectAnimator : AnimationSpriteClass
+{
+    [Header("Generics")]
+    public GhostVisionUIObject owner;
+
+
+    // private flags
+    protected bool _newActionRequested = false;
+
+    /*
+     * after the animation is done, go to next action
+     */
+    public void UpdateGhostAnimation() {
+        base.UpdateAnimation();
+        if (currentFrame + 1 >= animationStruct.Length) {
+            // prevent ghost disappearing with no despawn animation
+            if (! _newActionRequested) {
+                _newActionRequested = true;
+                owner.changeState();
+            }
+        }
+        else {
+            _newActionRequested = false;
+        }
+    }
+}
