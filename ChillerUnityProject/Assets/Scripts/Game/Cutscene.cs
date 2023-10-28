f7f6bf4ab32a57e04b55994b00760da39b78b4ed
theOldSheep <sunnymengyangliu@gmail.com>
2023-10-27 20:34:43 -0600

    cutscene implementation

--- ChillerUnityProject/Assets/Scripts/Game/Cutscene.cs
+++ ChillerUnityProject/Assets/Scripts/Game/Cutscene.cs
@@ -2,39 +2,152 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Video;
 
-public class Cutscene : MonoBehaviour
-{
-    // Start is called before the first frame update
+/*
+ * This is attatched to a video player object
+ * The management of a cutscene is done by such class
+ */
+public class Cutscene : UIObjectClass {
+    private const bool SHOULD_LOG_INFO = true; 
+    // identifier for a cutscene
+    public enum CutsceneID {
+        INTRO
+    }
+    // the enum that stores current cutscene state
+    public enum PlayPhase {
+        LOADING, PENDING_START, PLAYING, FINISHED
+    }
+    
 
-    public PlayableDirector playableDirector;
-    public Canvas canvas;
-    void Start()
-    {
-        playableDirector.Play();
+    [Header("Cutscene Basic Settings")]
+    // this is assigned in the inspector
+    public VideoPlayer attatchedCutscenePlayer;
+    // what scene to switch to after finishing playing the Cutscene?
+    public string sceneOnFinish = "";
+    public Vector3 posOnFinish = Vector3.zero;
+    [Header("Cutscene Identifier")]
+    public CutsceneID cutsceneIdentifier;
 
-        playableDirector.stopped += OnStopPlayingCutscene;
-        //playableDirector.time = 5.0f;
+
+
+    // internal variables
+    // this flag is here because the file might not be loaded when start is requested
+    protected bool _shouldStart = false,
+    // if the cutscene is paused because a menu is open etc. 
+    _isPaused = false,
+    // prevent excessive triggers on finishPlayingCutscene()
+    _finishTriggered = false;
+    // saves current playing phase (loading, pre-start, playing, ended)
+    protected PlayPhase _playingPhase = PlayPhase.LOADING;
+    public  PlayPhase playingPhase {
+        get {
+            return _playingPhase;
+        }
+    }
+    // internal dictionary that stores cutscenes
+    private static Dictionary<CutsceneID, Cutscene> _cutsceneMap = new Dictionary<CutsceneID, Cutscene>();
+
+
+    /*
+     * internal use: caches THIS CURRENT cutscene based on its identifier; recommended to have a single call on awake. 
+     */
+    protected void cacheCutscene() {
+        if (_cutsceneMap.ContainsKey(cutsceneIdentifier) ) {
+            Debug.Log("WARNING: possible duplication for cutscene " + cutsceneIdentifier + ". Do your cutscene have a wrong identifier?");
+        }
+        _cutsceneMap.Add(cutsceneIdentifier, this);
+    }
+    /*
+     * gets the cutscene cached with provided identifier.
+     */
+    public static Cutscene getCutsceneByID(CutsceneID identifier) {
+        if (_cutsceneMap.ContainsKey(identifier) ) {
+            return _cutsceneMap[identifier];
+        }
+        return null;
+    }
+    // hide when completed preparing
+    void afterCompletion(VideoPlayer cutscenePlayer) {
+        // if (SHOULD_LOG_INFO)
+        //     Debug.Log("Hiding Cutscene so it does not hide the title screen");
+        // IT SHOULD NOT COVER UP THE TITLE SCREEN!
+        // cutscenePlayer.enabled = false;
+    }
+    // overwrites
+    protected override void AwakeUIObject() {
+        cacheCutscene();
+        attatchedCutscenePlayer.Prepare();
+        attatchedCutscenePlayer.prepareCompleted += afterCompletion;
+    }
+    protected override void OnDestroyUIObject() {
+        finishPlayingCutscene();
+    }
+    protected override void UpdateUIObject() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(playableDirector.playableAsset.duration + ", " + playableDirector.time);
-        // just in case it does not stop
-        if (playableDirector.time > playableDirector.playableAsset.duration) {
-        // if (playableDirector.time > 5) {
-            playableDirector.Stop();
+    void Update() {
+        // play when the menu is not open
+        if(!MenuObjectClass.IsMenuActive()) {
+            switch (_playingPhase) {
+                // check if the loading is done
+                case PlayPhase.LOADING:
+                    if (attatchedCutscenePlayer.isPrepared) {
+                        if (SHOULD_LOG_INFO)
+                            Debug.Log("Cutscene " + cutsceneIdentifier + " is prepared and ready to launch");
+                        _playingPhase = PlayPhase.PENDING_START;
+                    }
+                    break;
+                // if the should start flag is true, start playing
+                case PlayPhase.PENDING_START:
+                    if (_shouldStart) {
+                        attatchedCutscenePlayer.Play();
+                        _playingPhase = PlayPhase.PLAYING;
+                        if (SHOULD_LOG_INFO)
+                            Debug.Log("Attempting to start playing Cutscene " + cutsceneIdentifier);
+                    }
+                    break;
+                // regularly check if the cutscene is finished playing
+                case PlayPhase.PLAYING:
+                    if (_isPaused) {
+                        _isPaused = false;
+                        attatchedCutscenePlayer.Play();
+                    }
+                    if ((! attatchedCutscenePlayer.isPlaying) ) {
+                        finishPlayingCutscene();
+                    }
+                    break;
+            }
         }
-        
+        // pause when menu is open
+        else {
+            _isPaused = true;
+            attatchedCutscenePlayer.Pause();
+        }
     }
 
-
-    void OnStopPlayingCutscene(PlayableDirector plDir) {
-        if (plDir == playableDirector) {
-            Debug.Log("o????");
-            GameManager.Instance.StartSwitchScene("HubRoom", Vector3.zero);
-            Debug.Log("o!!!!");
+    /*
+     * This is called when the cutscene should start playing
+     */
+    public void startPlayingCutscene() {
+        attatchedCutscenePlayer.renderMode = VideoRenderMode.CameraNearPlane;
+        _shouldStart = true;
+    }
+    /*
+     * This is called when the cutscene stops playing
+     */
+    public void finishPlayingCutscene() {
+        if (_finishTriggered)
+            return;
+        _finishTriggered = true;
+        _playingPhase = PlayPhase.FINISHED;
+        if (sceneOnFinish != "") {
+            GameManager.Instance.StartSwitchScene(sceneOnFinish, posOnFinish);
         }
+        // destroy the UI
+        UIObjectClass.DestroyUIObject(gameObject);
+        if (SHOULD_LOG_INFO)
+            Debug.Log("Cutscene " + cutsceneIdentifier + " was finished.");
     }
 }
